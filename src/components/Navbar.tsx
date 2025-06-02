7602e1a6bcf82f34cb56bd6a80458fbba815c877
cyber-warrior942 <usamazahoor942@gmail.com>
2025-06-02 07:06:42 +0500

    Rename app from Todo List to TaskMasterPro and update metadata

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,7 +12,7 @@ export default function Navbar() {
         <div className="flex justify-between h-16">
           <div className="flex items-center">
             <Link href="/" className="text-xl font-bold text-gray-800">
-              Todo List
+              TaskMasterPro
             </Link>
           </div>
           <div className="flex items-center space-x-4">
